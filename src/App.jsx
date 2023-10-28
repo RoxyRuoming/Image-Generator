2d7c31a24ad6217e19980523c980a1b5251f5830
RoxyRuoming <zhang.ruom@northeastern.edu>
2023-10-27 17:41:41 -0700

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,109 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import './index.css';
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 
 class App extends Component {
-  handleSubmit(e) {
+  state = {
+    isLoadingVisible: false,
+    val: '',
+    imgSrc: '',
+  };
+
+  showLoading = () => {
+    this.setState({ isLoadingVisible: true });
+  };
+
+  hideLoading = () => {
+    this.setState({ isLoadingVisible: false });
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
-    const prompt = e.target.searchQuery.value;
-   
+    this.showLoading();
+
     console.log(prompt);
     console.log(process.env.NODE_ENV);
-    document.querySelector('#overlay').style.display = 'block';
+
     const api =
       process.env.NODE_ENV === 'development'
         ? '/test/stabled'
         : 'https://grnioyrhj8.execute-api.us-east-2.amazonaws.com/test/stabled';
-    const data = { data: prompt };
+    const data = { data: e.target.searchQuery.value };
+    console.log(data);
     axios({
       method: 'POST',
       data: JSON.stringify(data),
       headers: { 'Content-Type': 'application/json' },
-      url: api,s
+      url: api,
     })
       .then((response) => {
         console.log(response);
-        const el = document.querySelector('#myImage');
-        el.setAttribute('src', response.data.body);
+        this.setState({ imgSrc: response.data.body });
+
         setTimeout(() => {
-          document.querySelector('#overlay').style.display = 'none';
-          const elem = document.getElementById('searchQuery');
-          elem.value = '';
-          elem.focus();
+          this.hideLoading();
+          this.setState({ val: '' });
         }, 500);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
   render() {
     return (
-      <div className='container'>
-        <div id='overlay'>
-          <div id='overlayText'>Loading your image please wait...</div>
-        </div>
-        <form onSubmit={this.handleSubmit}>
-          <h1> Welcome to AI Image Generator</h1>
-         
-          <input
-            autoFocus={true}
-            type='text'
-            name='searchQuery'
-            id='searchQuery'
-            placeholder='enter desired text'
-            required
-          />
-          <div>
-            <input type='submit' value='Submit' />
-          </div>
-          <div>
-            <br></br>
-            <img
-              id='myImage'
-              alt='Your Image will appear here'
-              className='imageContainer'
+      <Container className='p-5' id='container' name='container'>
+        <h1> Welcome to Stable Diffusion AI</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Label>Enter Text to convert Image</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Enter text to convert image'
+              required
+              autoFocus={true}
+              name='searchQuery'
+              controlId='searchQuery'
+              defaultValue={this.state.val}
             />
+            <Form.Text className='text-muted'>
+              We'll attempt our best to create image you will love it!
+            </Form.Text>
+          </Form.Group>
+          <Button
+            variant='primary'
+            type='submit'
+            className='btn btn-primary btn-large centerButton'
+          >
+            Submit
+          </Button>
+
+          <Image
+            id='myImage'
+            className='img-fluid shadow-4'
+            src={this.state.imgSrc}
+          />
+        </Form>
+        {this.state.isLoadingVisible && (
+          <div id='backdrop'>
+            <Button variant='primary' disabled>
+              <Spinner
+                target='container'
+                as='span'
+                animation='grow'
+                size='sm'
+                role='status'
+                aria-hidden='true'
+              />
+              Loading...
+            </Button>
           </div>
-        </form>
-      </div>
+        )}
+      </Container>
     );
   }
 }
