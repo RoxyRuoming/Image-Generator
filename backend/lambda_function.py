384e0a16e130c3a7c077a3787640d92f74ff7ba9
RoxyRuoming <zhang.ruom@northeastern.edu>
2023-10-27 20:51:43 -0700

    modify lambda_function.py

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -58,7 +58,8 @@ def lambda_handler(event, context):
         'headers': {
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+            'Content-Type': 'application/json'
+        }
         'body': url
     }
